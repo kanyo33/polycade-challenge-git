f19ae69adce39e6c12671eb15aeee122bc321652
Noah <n-k@live.ca>
2020-02-25 09:26:12 +0200

    commit changes

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
-<<<<<<< HEAD
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
 
-=======
->>>>>>> 8f29a10fdbcef5836d13082de027b09b593a024b
   if (req.url === '/user-agent') return respondUserAgent(req, res)
 
   res.end()
@@ -18,14 +15,11 @@ function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
-<<<<<<< HEAD
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
 }
 
-=======
->>>>>>> 8f29a10fdbcef5836d13082de027b09b593a024b
 function respondUserAgent (req, res) {
   const ua = req.headers['user-agent']
   res.end(JSON.stringify({ ua }))
